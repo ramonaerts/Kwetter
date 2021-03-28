275b744eaa06f1702a970985be4b848e63cb7e3f
ramonaerts <ramon.aerts@gmail.com>
2021-03-28 21:06:49 +0200

    fixed small issues with loading tweets

--- TweetService/Services/TweetService.cs
+++ TweetService/Services/TweetService.cs
@@ -22,7 +22,7 @@ namespace TweetService.Services
             tweets.Add(new Tweet
             {
                 TweetId = 1,
-                TweetContent = "Test tweet.",
+                TweetContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
                 UserId = 1,
                 TweetDateTime = DateTime.Now.AddHours(-4)
             });
@@ -44,7 +44,7 @@ namespace TweetService.Services
 
         public List<Tweet> GetTweets()
         {
-            FillMockTweets();
+            if (tweets.Count == 0) FillMockTweets();
 
             foreach (var tweet in tweets) tweet.User = users.Find(e => e.UserId == tweet.UserId);
 
