ecac75640057af5abe870f619dea5bbf98c1e533
ramonaerts <ramon.aerts@gmail.com>
2021-04-16 13:29:16 +0200

    created userservice

---
+++ UserService/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Shared.API;
+using UserService.Messages.API;
+
+namespace UserService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        public UserController()
+        {
+            
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public ApiResult RegisterUser(RegisterMessage message) 
+        {
+            return ApiResult.Success("success");
+        }
+    }
+}
