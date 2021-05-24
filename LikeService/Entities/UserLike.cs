9eabb28db21c561b825f7c3f91572145a9677e3b
ramonaerts <ramon.aerts@gmail.com>
2021-05-24 19:39:19 +0200

    updated shared version for all services

--- LikeService/Entities/UserLike.cs
+++ LikeService/Entities/UserLike.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace LikeService.Entities
 {
     public class UserLike
     {
+        [BsonId]
         public string Id { get; set; }
         public string UserId { get; set; }
         public List<string> UserLikes { get; set; }
--- LikeService/Program.cs
+++ LikeService/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Logging;
 
 namespace LikeService
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
