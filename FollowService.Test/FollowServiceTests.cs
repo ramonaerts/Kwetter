4d41a8c941563080fb3a2201499ce2ade03fc10e
ramonaerts <ramon.aerts@gmail.com>
2021-05-26 20:21:31 +0200

    created mocks for followservice tests

--- FollowService.Test/FollowServiceTests.cs
+++ FollowService.Test/FollowServiceTests.cs
@@ -1,10 +1,19 @@
 using System;
+using FollowService.Test.Mock;
 using Xunit;
 
 namespace FollowService.Test
 {
     public class FollowServiceTests
     {
+        private readonly Services.FollowService _followService;
+
+        public FollowServiceTests()
+        {
+            var mongoRepositoryMock = new MongoRepositoryMock();
+            _followService = new Services.FollowService(new MessagePublisherMock(), mongoRepositoryMock);
+        }
+
         [Fact]
         public void Test1()
         {
---
+++ FollowService.Test/Mock/MongoRepositoryMock.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using FollowService.DAL;
+using FollowService.Entities;
+
+namespace FollowService.Test.Mock
+{
+    public class MongoRepositoryMock : IFollowRepository
+    {
+        public List<Follow> Follows;
+
+        public MongoRepositoryMock()
+        {
+            Follows = new List<Follow>();
+        }
+
+        public Follow GetFollow(string followerId, string followingId)
+        {
+            return Follows.Find(f => f.Follower == followerId && f.Following == followingId);
+        }
+
+        public Task AddOneFollower(Follow follow)
+        {
+            Follows.Add(follow);
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteOneFollower(string followerId, string followingId)
+        {
+            Follows.RemoveAll(f => f.Follower == followerId && f.Following == followingId);
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteAllFollowers(string userId)
+        {
+            Follows.RemoveAll(f => f.Follower == userId);
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteAllFollowing(string userId)
+        {
+            Follows.RemoveAll(f => f.Following == userId);
+            return Task.CompletedTask;
+        }
+    }
+}
